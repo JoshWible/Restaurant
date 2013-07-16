5614976b3d084f5bc03e94f072a912117f3c1d0b
Jkoellner <Jkoellner@gmail.com>
2013-07-16 09:23:58 -0400

    initial commit

---
+++ ROS/Kitchen.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Kitchen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Kitchen
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Kitchen
+     */
+    public Kitchen()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
